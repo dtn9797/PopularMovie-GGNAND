fd79df2ceb439e0499793a82a08d019208b66ffc
Duy Nguyen <dtn9797@gmail.com>
2018-05-28 00:36:52 -0500

    Middle of creating ViewHolder.java as a seperate class

---
+++ app/src/main/java/com/example/duynguyen/movieapp/Utils/ViewHolder.java
@@ -0,0 +1,38 @@
+package com.example.duynguyen.movieapp.Utils;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.example.duynguyen.movieapp.Model.Movie;
+import com.example.duynguyen.movieapp.R;
+import com.squareup.picasso.Picasso;
+
+public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public RelativeLayout relativeLayout;
+    public ImageView imageView;
+    Movie item;
+
+
+    public ViewHolder(View v) {
+        super(v);
+
+        v.setOnClickListener(this);
+        relativeLayout = (RelativeLayout) v.findViewById(R.id.relativeLayout);
+        imageView = (ImageView) v.findViewById(R.id.imageView);
+    }
+
+    public void setData (Movie item) {
+        this.item = item;
+
+        Picasso.get().load("http://image.tmdb.org/t/p/w185"+item.getPoster_path()).into(imageView);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (RecyclerViewAdapter.mItemClickListener!=null){
+            RecyclerViewAdapter.mItemClickListener.onItemClick(item);
+        }
+    }
+}
