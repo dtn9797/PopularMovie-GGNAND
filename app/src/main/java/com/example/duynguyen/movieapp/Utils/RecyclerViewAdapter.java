4bf932f93a6fbc3e495f147b46afb954176d06c5
Duy Nguyen <dtn9797@gmail.com>
2018-05-28 00:30:49 -0500

    Middle of creating ViewHolder.java as a seperate class

--- app/src/main/java/com/example/duynguyen/movieapp/Utils/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/duynguyen/movieapp/Utils/RecyclerViewAdapter.java
@@ -14,11 +14,11 @@ import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<ViewHolder> {
 
     ArrayList<Movie> mItems=new ArrayList<Movie>();
     Context mContext;
-    ItemListener mItemClickListener;
+    protected static ItemListener mItemClickListener;
 
     public RecyclerViewAdapter(Context context, ItemListener itemClickListener){
         mContext = context;
@@ -26,33 +26,33 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        public RelativeLayout relativeLayout;
-        public ImageView imageView;
-        Movie item;
-
-
-        public ViewHolder(View v) {
-            super(v);
-
-            v.setOnClickListener(this);
-            relativeLayout = (RelativeLayout) v.findViewById(R.id.relativeLayout);
-            imageView = (ImageView) v.findViewById(R.id.imageView);
-        }
-
-        public void setData (Movie item) {
-            this.item = item;
-
-            Picasso.get().load("http://image.tmdb.org/t/p/w185"+item.getPoster_path()).into(imageView);
-        }
-
-        @Override
-        public void onClick(View v) {
-            if (mItemClickListener!=null){
-                mItemClickListener.onItemClick(item);
-            }
-        }
-    }
+//    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+//        public RelativeLayout relativeLayout;
+//        public ImageView imageView;
+//        Movie item;
+//
+//
+//        public ViewHolder(View v) {
+//            super(v);
+//
+//            v.setOnClickListener(this);
+//            relativeLayout = (RelativeLayout) v.findViewById(R.id.relativeLayout);
+//            imageView = (ImageView) v.findViewById(R.id.imageView);
+//        }
+//
+//        public void setData (Movie item) {
+//            this.item = item;
+//
+//            Picasso.get().load("http://image.tmdb.org/t/p/w185"+item.getPoster_path()).into(imageView);
+//        }
+//
+//        @Override
+//        public void onClick(View v) {
+//            if (mItemClickListener!=null){
+//                mItemClickListener.onItemClick(item);
+//            }
+//        }
+//    }
 
     public void setMoviesData (ArrayList<Movie> movies){
         mItems= movies;
@@ -60,16 +60,17 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     @Override
-    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         //Context context = parent.getContext();
         View view = LayoutInflater.from(mContext).inflate(R.layout.content_main, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerViewAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
         holder.setData(mItems.get(position));
     }
+
 
     @Override
     public int getItemCount() {
