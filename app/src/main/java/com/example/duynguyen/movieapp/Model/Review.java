524608a97301f3c22130bb1b751e058927349f9f
Duy Nguyen <dtn9797@gmail.com>
2018-06-06 00:23:46 -0500

    fix a bug to match correct name from json

--- app/src/main/java/com/example/duynguyen/movieapp/Model/Review.java
+++ app/src/main/java/com/example/duynguyen/movieapp/Model/Review.java
@@ -1,11 +1,15 @@
 package com.example.duynguyen.movieapp.Model;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by duynguyen on 6/5/18.
  */
 
 public class Review {
+    @SerializedName("author")
     private String author;
+    @SerializedName("content")
     private String detail;
 
     public String getAuthor() {
