a55754278f5c15a6fe0ea9b9eb5743f0635107eb
Duy Nguyen <dtn9797@gmail.com>
2018-06-09 22:41:36 -0500

    Edited movie for ROOM

--- app/src/main/java/com/example/duynguyen/movieapp/Model/Movie.java
+++ app/src/main/java/com/example/duynguyen/movieapp/Model/Movie.java
@@ -1,12 +1,18 @@
 package com.example.duynguyen.movieapp.Model;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
 import com.google.gson.annotations.SerializedName;
 
 /**
  * Created by duynguyen on 5/15/18.
  */
-
+@Entity(tableName = "movie")
 public class Movie {
+    @PrimaryKey
+    @SerializedName("id")
+    private String id;
     @SerializedName("poster_path")
     private String posterPath;
     @SerializedName("vote_average")
@@ -17,8 +23,15 @@ public class Movie {
     private String releaseDate;
     @SerializedName("original_title")
     private String title;
-    @SerializedName("id")
-    private String id;
+
+    public Movie(String id, String posterPath, String voteAverage, String overview, String releaseDate, String title) {
+        this.id = id;
+        this.posterPath = posterPath;
+        this.voteAverage = voteAverage;
+        this.overview = overview;
+        this.releaseDate = releaseDate;
+        this.title = title;
+    }
 
     public String getId() {
         return id;
